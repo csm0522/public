a72fe0a7ac163fde83859c8820e70a4e38887223
Chan <447880522@qq.com>
2016-06-16 17:30:14 +0800

    口水区修改

--- share/Application/Home/Controller/KsqController.class.php
+++ share/Application/Home/Controller/KsqController.class.php
@@ -5,4 +5,30 @@ class KsqController extends Controller {
     public function index(){
     	$this->display();
     }
+    public function addTopic(){
+        $sessid = session('userInfo.UId');
+        if($sessid==""){
+            echo '<script>alert("请先登录");window.history.go(-1); </script>';
+        }
+        else{
+//            echo $sessid;
+            if(empty($_POST)){
+                echo  '<script>alert("请填写吐槽内容哟~");window.history.go(-1); </script>';
+            }
+            else{
+                $data['word']=$_POST['sword'];
+                $data['id']=$sessid;
+                $data['time']=Date('Y-m-d H:i:s');
+                $data['type']=$_POST['types'];
+                $rs = D('User')->addTopic($data);
+                if($rs==0){
+                    echo  '<script>alert("发布成功");window.history.go(-1); </script>';
+                }
+                else{
+                    echo  '<script>alert("发布失败,请稍后重试");window.history.go(-1); </script>';
+                }
+            }
+        }
+
+    }
 }
--- share/Application/Home/Model/UserModel.class.php
+++ share/Application/Home/Model/UserModel.class.php
@@ -26,6 +26,24 @@ class UserModel extends Model{
             array('login_un','require','用户名必须填写' ),
             array('login_pwd','require','密码必须填写' )
     );
+    //添加话题标题
+    function addTopic($data=array()){
+        $datas=array("","");
+        $condition['LoginId']=$data['id'];
+        $userid=M('user')->where($condition)->getfield(UserId);
+        $datas['UserId']=$userid;
+        $datas['title']=$data['word'];
+        $datas['CreatTime']=$data['time'];
+        $datas['type']=$data['type'];
+        $datas['ReportTag']=0;
+        $Topic=M('topic');
+        if($Topic->add($datas)){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
 
 }
 
