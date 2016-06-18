74c7840b8c7ef069271cb33a41d02264ae7cb26f
Chan <447880522@qq.com>
2016-06-18 10:23:13 +0800

    有一个问题～～～个人中心发布作品之后就乱了

--- share/Application/Home/Controller/IndexController.class.php
+++ share/Application/Home/Controller/IndexController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
 		$sessid = session('userInfo.UId');
-    	$Works = M('artical') -> join('t_user on t_artical.userid = t_user.userid') -> where('upLoadType = 1') ->select();
+    	$Works = M('artical') -> join('t_user on t_artical.userid = t_user.userid') -> where('upLoadType = 1')->order("AriticalId DESC") ->select();
     	$Artical = M('artical') -> join('t_user on t_artical.userid = t_user.userid') -> where('upLoadType = 2') ->select();
 		$userTX = M('user')-> where("loginid = '$sessid'")-> getfield('usertx');
 		$this->assign("Workslist",$Works);
